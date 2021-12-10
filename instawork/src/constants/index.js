f072cdcf547fdb3ba0dfb3cf52e37356eff80da0
AdityaSatnalika0202 <89994257+AdityaSatnalika0202@users.noreply.github.com>
2021-12-10 13:08:43 -0500

    heroku backend

--- instawork/src/constants/index.js
+++ instawork/src/constants/index.js
@@ -1,4 +1,4 @@
-export const baseurl = "http://127.0.0.1:5000/";
+export const baseurl = "https://instawork-backend.herokuapp.com/";
 export const list = "list";
 export const add = "add";
 export const deleteUser = "delete";
