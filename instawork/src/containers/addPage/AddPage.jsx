336a522083903f5596ce407123bb8bc5762adee0
AdityaSatnalika0202 <89994257+AdityaSatnalika0202@users.noreply.github.com>
2021-12-10 11:18:09 -0500

    bugs fix

--- instawork/src/containers/addPage/AddPage.jsx
+++ instawork/src/containers/addPage/AddPage.jsx
@@ -3,6 +3,8 @@ import './addpage.css'
 import Header from '../../components/header/Header';
 import Description  from '../../components/description/Description'
 import UserInfoForm from '../../components/userInfoForm/UserInfoForm';
+import { ArrowLeftCircle } from 'react-bootstrap-icons';
+import { useDispatch } from "react-redux";
 
 const AddPage = () => {
     const user = {
@@ -11,11 +13,20 @@ const AddPage = () => {
         "email" : "",
         "contact" : ""
       }
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        dispatch({
+            type: "SCREEN",
+            screen: "list"
+        })
+    }
     return (
         <div className='container-placeholder'>
             <Header headerName = "Add Page" />
             <div className='main-container'>
                 <div className='sub-container'>
+                    <div className='icon-edit-page'><ArrowLeftCircle size={25} onClick={handleClick}/></div>
                     <Description mainHeading = {"Add a team member"} subHeading = {"Set email, location and role"}/>
                     <UserInfoForm  user = {user}/>
                 </div>
